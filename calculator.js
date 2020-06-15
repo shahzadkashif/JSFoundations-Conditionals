fb5f42dffb1e5fefacd3e710dbd7a1c6d19a2314
Kashif <kashifshahzad@yours.com>
2020-06-15 19:50:25 +0300

    Calculator JavaScript

--- calculator.js
+++ calculator.js
@@ -1,3 +1,18 @@
 // Define the numbers and the operation here
+let Num1 = 5;
+let Num2 = 3;
+let operator = "+";
+let result;
 
 // Write if-statements here
+
+if (operator === "+") {
+  result = Num1 + Num2;
+} else if (operator === "-") {
+  result = Num1 - Num2;
+} else if (operator === "/") {
+  result = Num1 / Num2;
+} else if (operator === "*") {
+  result = Num1 * Num2;
+}
+console.log(result);
